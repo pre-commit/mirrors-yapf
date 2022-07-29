208a008f7bb1bdaecbf07085a93eecd5cc12029f
jefer94 <jdefreitaspinto@gmail.com>
2022-07-29 01:12:34 -0500

    add support to toml

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['yapf==0.32.0'],
+    install_requires=['yapf==0.32.0', 'toml==0.10.2'],
 )
